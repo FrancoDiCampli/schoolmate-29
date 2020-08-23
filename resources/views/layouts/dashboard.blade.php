041d0a7611675116cdcb1d2c9fe99160650ae4eb
pelleranogiovanni <pelleranogiovanni@hotmail.com>
2020-08-22 21:18:26 -0300

    language spanish tiny

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="{{ asset('css/main.css?v='.filemtime(public_path('css/main.css'))) }}"/>
     <script src="https://cdn.tiny.cloud/1/vinu6fycgkkxj45zegjm1xtojyx5ttlt8c5mgbghx4knbh2a/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <script>
-    tinymce.init({selector:'textarea', languaje: 'es'});
+    tinymce.init({selector:'textarea', language: 'es'});
     </script>
 </head>
 
