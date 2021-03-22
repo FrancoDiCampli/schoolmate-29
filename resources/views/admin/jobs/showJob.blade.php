1c340a306364a8fadc54efcc478ea4c9e13d9c7b
germanio7 <german.e.escobar@gmail.com>
2021-03-22 17:33:09 -0300

    fix password confirm

--- resources/views/admin/jobs/showJob.blade.php
+++ resources/views/admin/jobs/showJob.blade.php
@@ -58,29 +58,26 @@
             </div>
 
             {{-- Descargar y eliminar --}}
-            @if ($job->state($job->state) === "Activa")
+            @if ($entregas)
             <a class="btn btn-default mr-5 py-1 px-2 rounded-md hidden md:flex items-center"
-                href="{{route('descargarEntregas', $job->id)}}" title="Descargar todas las entregas">                
-                <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+                href="{{route('descargarEntregas', $job->id)}}" title="Descargar todas las entregas">
+                <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
                 </svg>
                 <span class="hidden lg:block ml-2">Entregas</span>
             </a>
-
-            {{-- @if ($job->download) --}}
-            <form action="{{route('eliminarEntregas')}}" method="POST"
-                onsubmit="return confirm('¿Desea continuar? Se eliminarán los archivos de la tarea y sus correspondientes entregas. Esta acción es irreversible.')">
-                @csrf
-                <input type="hidden" name="id" id="" value="{{$job->id}}">
-                <button class="btn btn-default mr-5 py-1 px-2 rounded-md hidden md:flex items-center" type="submit" title="Eliminar todos los archivos">
-                    <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                    </svg>
-                    <span class="hidden lg:block ml-2">Archivos</span>
-                </button>
-            </form>
-            {{-- @endif --}}
-
+            <a href="{{route('eliminarEntregas', $job->id)}}"
+                class="btn btn-default mr-5 py-1 px-2 rounded-md hidden md:flex items-center"
+                title="Eliminar todos los archivos">
+                <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+                <span class="hidden lg:block ml-2">Archivos</span>
+            </a>
             @endif
 
             @if ($job->file_path)
